111193b6b28ca4f43d3950b57cfaeb160143a482
Aditya Budhathoki <budhathokiaditya21@gmail.com>
2024-12-21 11:56:45 +0545

    Update qno10.py

--- qno10.py
+++ qno10.py
@@ -1,7 +1,5 @@
-# Input marks from the user
 marks = float(input("Enter the marks: "))
 
-# Determine the grade
 if marks >= 90 and marks <= 100:
     grade = 'A'
 elif marks >= 80 and marks < 90:
@@ -10,6 +8,4 @@ elif marks >= 70 and marks < 80:
     grade = 'C'
 else:
     grade = 'Fail'
-
-# Output the grade
 print(f"The grade is: {grade}")
