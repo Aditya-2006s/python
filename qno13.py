ad4809af8c1c4bd83bd63dabe9d7a556819d5ea0
Aditya Budhathoki <budhathokiaditya21@gmail.com>
2024-12-21 11:58:07 +0545

    Update qno13.py

--- qno13.py
+++ qno13.py
@@ -1,7 +1,4 @@
-# Input color from the user
 color = input("Enter a color (Red, Yellow, Green): ").capitalize()
-
-# Determine the corresponding action based on the color
 if color == "Red":
     action = "Stop"
 elif color == "Yellow":
@@ -11,5 +8,4 @@ elif color == "Green":
 else:
     action = "Invalid color"
 
-# Output the action
 print(f"The action is: {action}")
