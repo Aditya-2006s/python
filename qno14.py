c1a337304e3335c85028a81f41cf8418bcfc6fc4
Aditya Budhathoki <budhathokiaditya21@gmail.com>
2024-12-21 11:58:35 +0545

    Update qno14.py

--- qno14.py
+++ qno14.py
@@ -1,12 +1,9 @@
-# Input age and experience from the user
 age = int(input("Enter your age: "))
 experience = int(input("Enter your experience in years: "))
 
-# Check eligibility based on age and experience
 if age > 18 and experience >= 2:
     eligibility = "Eligible"
 else:
     eligibility = "Not Eligible"
 
-# Output the eligibility status
 print(f"You are {eligibility} for the job.")
