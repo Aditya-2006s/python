b1712de35b049a2f29a7c317f913c8de911357fc
Aditya Budhathoki <budhathokiaditya21@gmail.com>
2024-12-21 11:59:37 +0545

    Update qno16.py

--- qno16.py
+++ qno16.py
@@ -1,7 +1,5 @@
-# Input menu option from the user
 menu_option = input("Enter a menu option (Pizza, Burger, Pasta): ").capitalize()
 
-# Determine the price based on the menu option
 if menu_option == "Pizza":
     price = "$10"
 elif menu_option == "Burger":
@@ -11,5 +9,4 @@ elif menu_option == "Pasta":
 else:
     price = "Invalid option"
 
-# Output the price
 print(f"The price of {menu_option} is: {price}")
