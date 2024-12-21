d337506ca53a5945f56b9bcb64b6bf4c7ccf6c4b
Aditya Budhathoki <budhathokiaditya21@gmail.com>
2024-12-21 11:56:03 +0545

    Update qno19.py

--- qno19.py
+++ qno19.py
@@ -1,8 +1,6 @@
-# Input username and password from the user
 username = input("Enter your username: ")
 password = input("Enter your password: ")
 
-# Check if the credentials are correct
 if username == "admin" and password == "password123":
     print("Access Granted")
 else:
