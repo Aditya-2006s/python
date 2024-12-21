ffb09cedffa65221dcba6e7acf1cfb6df1003f88
Aditya Budhathoki <budhathokiaditya21@gmail.com>
2024-12-21 12:00:12 +0545

    Update qno17.py

--- qno17.py
+++ qno17.py
@@ -1,11 +1,9 @@
-# Input height from the user
 height = float(input("Enter your height in feet: "))
 
-# Select players based on height
 if height >= 6:
     selection = "Selected"
 else:
     selection = "Not Selected"
 
-# Output the result
+
 print(f"You are {selection}.")
